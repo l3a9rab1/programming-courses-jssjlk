ec7a7fbf98a025ecef42feb1c767812f369f8c8a
l3a9rab1 <138827476+l3a9rab1@users.noreply.github.com>
2023-07-07 20:53:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-jssjlk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-jssjlk)
---
+++ index.ts
@@ -0,0 +1,15 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let ronaldo: number;
+let messi: number;
+let hakimi: number;
+let ziyach: number;
+let sabiri: number;
+ronaldo = 7;
+messi = 10;
+hakimi = 6;
+ziyach = 7;
+sabiri = ronaldo + messi + hakimi + ziyach;
+console.log(sabiri);
